14eee4609b4cbfafdf65b63c7c8fc1655e4c0166
Mathew Joseph <mathewjoseph200526@gmail.com>
2024-03-14 09:00:43 +0530

    Update experiment5.c

--- labwork/experiment5.c
+++ labwork/experiment5.c
@@ -9,14 +9,6 @@ int main() {
 
     originalNum = num;
 
-
-    while (originalNum != 0) {
-        originalNum /= 10;
-        ++n;
-    }
-
-    originalNum = num;
-
     
     while (originalNum != 0) {
         remainder = originalNum % 10;
